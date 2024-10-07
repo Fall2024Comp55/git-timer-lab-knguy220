28e32c4c4290fcab5433bb0ece4ae91bb8ef3751
Kennedy Nguyen <k_nguyen198@u.pacific.edu>
2024-10-06 22:29:23 -0700

    all balls move across the screen

--- src/BallLauncher.java
+++ src/BallLauncher.java
@@ -1,14 +1,22 @@
 import acm.graphics.*;
 import acm.program.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
-import javax.swing.*;
+import javax.swing.*; 
+import java.awt.event.*; 
+import java.util.*; 
+import java.awt.Color;
+import javax.swing.Timer;
 
-public class BallLauncher extends GraphicsProgram{
+public class BallLauncher extends GraphicsProgram implements ActionListener {
+	
+	public static final int SIZE = 25;
+	public static final int MS = 50;
+	public static final int SPEED = 2;
+	
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
-	public static final int SIZE = 25;
+	
+	private ArrayList<GOval> balls;
+	private Timer moveTimer;
 	
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -16,12 +24,16 @@ public class BallLauncher extends GraphicsProgram{
 	}
 	
 	public void run() {
+		balls = new ArrayList<>();
 		addMouseListeners();
+		moveTimer = new Timer(MS, this);
+		moveTimer.start();
 	}
 	
 	public void mousePressed(MouseEvent e) {
 		GOval ball = makeBall(SIZE/2, e.getY());
 		add(ball);
+		balls.add(ball);
 	}
 	
 	public GOval makeBall(double x, double y) {
@@ -30,6 +42,13 @@ public class BallLauncher extends GraphicsProgram{
 		temp.setFilled(true);
 		return temp;
 	}
+	
+	@Override
+    public void actionPerformed(ActionEvent e) {
+        for (GOval ball : balls) {
+            ball.move(SPEED, 0); 
+        }
+    }
 	
 	public static void main(String[] args) {
 		new BallLauncher().start();
