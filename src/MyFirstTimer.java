dd45fcf96c7512dfc76e5e440b965c4d512bd48b
Kennedy Nguyen <k_nguyen198@u.pacific.edu>
2024-10-06 22:16:32 -0700

    Added timer: setup, start, handle action

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,15 @@
 import acm.graphics.*;
 import acm.program.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class MyFirstTimer extends GraphicsProgram {
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private Timer myTimer;
+	private int numTimes;
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -13,9 +17,30 @@ public class MyFirstTimer extends GraphicsProgram {
 	}
 	
 	public void run() {
+		numTimes = 0;
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		
+		myTimer = new Timer(1000, this);
+        myTimer.setInitialDelay(3000);
+		myTimer.start();
+
 	}
+	
+	@Override
+    public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		myLabel.setLabel("times called? " + numTimes);
+
+		
+		if (numTimes == 10) {
+            myTimer.stop();
+            return; 
+        }
+		
+		myLabel.move(5, 0);
+
+    }
 	
 	public static void main(String[] args) {
 		new MyFirstTimer().start();
